b855f518869ba1684736fc4f676939b789da0e83
Stackofsugar <digno.christopher@gmail.com>
2020-12-13 21:16:41 +0700

    add files

---
+++ etc/input_validation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+void cleanbuf(){
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+}
+
+template<typename Ty>
+Ty input(Ty floor = 0, Ty ceil = hardLimit){
+    Ty buffer;
+    while(1){
+        if(cin >> buffer){
+            if(buffer < floor){
+                cout << "Your input was too small! Try again: \n";
+            } else if(buffer > ceil){
+                cout << "Your input was too big! Try again: \n";
+            } else {
+                return buffer;
+            }
+        } else {
+            cout << "Invalid input! Please try again: \n";
+            cleanbuf();
+        }
+    }
+}
